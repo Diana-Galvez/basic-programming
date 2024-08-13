a3326511ebb945b220c852e781725cca66fd73d8
Diana-Galvez <dianagalvez739@gmail.com>
2024-08-12 21:34:53 -0500

    the user is prevented from typing anything, converting the input from string to number

--- 01-variables-printf-strings/main.go
+++ 01-variables-printf-strings/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 var options string = `
@@ -29,35 +30,43 @@ func main() {
 	fmt.Println(options)
 	input, _ = reader.ReadString('\n')
 	input = input[:len(input)-1]
-  fmt.Println(input)
-	/*var age float64 = 0
-	var name string = ""
-	var lastName string = ""
-
-  // indicamos al usuario lo que debe escribir y al final dar a enter, para que
-	// quede guardado en la variable name
-	fmt.Print("escribe tu nombre: ")
-	// Una vez finalice el mensaje darle a enter
-	name, _ = reader.ReadString('\n')
-	name = name[:len(name)-1]
-
-	// Te pide escribir tu apellido
-	fmt.Print("escribe tu apellido: ")
-	// Una vez hecho darle enter para que quede guardo en la variable lastName
-  lastName, _ = reader.ReadString('\n')
-	lastName = lastName[:len(lastName)-1]
-
-
 
 	// Declaramos una variable age y una variable err,
 	// y con ParseFloat recibimos el string "texto" y también recibe el grado de precisión
 	// convirtiéndolo en decimal.
-	age, err := strconv.ParseFloat(input, 2)
+	option, err := strconv.Atoi(input)
 	if err != nil {
-		panic("Debes escribir un número decimal (ex. 32.6)")
+		panic("Debes escribir un número entre el 1 y el 12")
 	}
+	fmt.Println(option)
+	/*var age float64 = 0
+		var name string = ""
+		var lastName string = ""
 
-// ¡Hola! <name> <lastname>. Tu edad en días es <age (days)>, en semanas es de <age (weeks)> y en meses es de <age (months)>
-  fmt.Printf("¡Hola!, %s %s. Tu edad en días es %.2f, en semanas es de %.2f y en meses es de %.2f \n",
-		 name, lastName, age * 365, age * 52, age * 12)*/
+	  // indicamos al usuario lo que debe escribir y al final dar a enter, para que
+		// quede guardado en la variable name
+		fmt.Print("escribe tu nombre: ")
+		// Una vez finalice el mensaje darle a enter
+		name, _ = reader.ReadString('\n')
+		name = name[:len(name)-1]
+
+		// Te pide escribir tu apellido
+		fmt.Print("escribe tu apellido: ")
+		// Una vez hecho darle enter para que quede guardo en la variable lastName
+	  lastName, _ = reader.ReadString('\n')
+		lastName = lastName[:len(lastName)-1]
+
+
+
+		// Declaramos una variable age y una variable err,
+		// y con ParseFloat recibimos el string "texto" y también recibe el grado de precisión
+		// convirtiéndolo en decimal.
+		age, err := strconv.ParseFloat(input, 2)
+		if err != nil {
+			panic("Debes escribir un número decimal (ex. 32.6)")
+		}
+
+	// ¡Hola! <name> <lastname>. Tu edad en días es <age (days)>, en semanas es de <age (weeks)> y en meses es de <age (months)>
+	  fmt.Printf("¡Hola!, %s %s. Tu edad en días es %.2f, en semanas es de %.2f y en meses es de %.2f \n",
+			 name, lastName, age * 365, age * 52, age * 12)*/
 }
